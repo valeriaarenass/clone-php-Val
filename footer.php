bf8ffa5b5ef468984f4c54f284f3d163a2184ac4
Valeria Arenas Salgado <valeriaaarenass@gmail.com>
2021-03-02 21:04:22 -0500

    first commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+        <div>
+            <address>
+                Autor: Amilcar Gómez
+                Corrreo <a href="mailto:amilcar@compuestudio.edu.co">amilcar@compuestudio.edu.co</a>
+            </address>
+        </div>
+</footer>
---
+++ head.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+    *Autor: Amilcar Gomez
+    *Fecha: 02-03-2021
+*/
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Amilcar Gómez">
+    <meta name="description" content="Pagina Demo de Estudio con PHP">
+</head>
